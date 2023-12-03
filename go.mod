9def897b1354ac2807e4e8068c5bb6a641e6250e
Daffa Audya Pramana <daffaaudyapramana03@gmail.com>
2023-12-04 03:41:00 +0700

    add

---
+++ go.mod
@@ -0,0 +1,48 @@
+module github.com/wegotour/webhooks
+
+go 1.21.4
+
+require (
+	github.com/aiteung/atapi v0.1.1
+	github.com/aiteung/atmessage v0.1.0
+	github.com/aiteung/module v0.1.0
+	github.com/whatsauth/wa v0.2.6
+)
+
+require (
+	aidanwoods.dev/go-paseto v1.5.0 // indirect
+	aidanwoods.dev/go-result v0.1.0 // indirect
+	filippo.io/edwards25519 v1.0.0 // indirect
+	github.com/Rhymen/go-whatsapp v0.1.1 // indirect
+	github.com/aiteung/atdb v0.1.3 // indirect
+	github.com/aiteung/musik v0.0.5 // indirect
+	github.com/go-sql-driver/mysql v1.7.1 // indirect
+	github.com/golang-sql/civil v0.0.0-20220223132316-b832511892a9 // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
+	github.com/google/go-cmp v0.5.9 // indirect
+	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/google/uuid v1.4.0 // indirect
+	github.com/gorilla/websocket v1.5.1 // indirect
+	github.com/klauspost/compress v1.17.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/mattn/go-sqlite3 v1.14.18 // indirect
+	github.com/microsoft/go-mssqldb v1.6.0 // indirect
+	github.com/montanaflynn/stats v0.7.1 // indirect
+	github.com/rs/xid v1.5.0 // indirect
+	github.com/whatsauth/watoken v0.1.3 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.1.2 // indirect
+	github.com/xdg-go/stringprep v1.0.4 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20201027041543-1326539a0a0a // indirect
+	go.mau.fi/libsignal v0.1.0 // indirect
+	go.mau.fi/util v0.2.0 // indirect
+	go.mau.fi/whatsmeow v0.0.0-20231112141858-68b25b3f6fe9 // indirect
+	go.mongodb.org/mongo-driver v1.12.1 // indirect
+	golang.org/x/crypto v0.15.0 // indirect
+	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/sync v0.5.0 // indirect
+	golang.org/x/sys v0.14.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/protobuf v1.31.0 // indirect
+)
---
+++ handler.go
@@ -0,0 +1,118 @@
+package webhook
+
+import (
+	"encoding/json"
+	"fmt"
+	"math/rand"
+	"net/http"
+	"os"
+	"strconv"
+
+	"github.com/aiteung/atapi"
+	"github.com/aiteung/atmessage"
+	"github.com/aiteung/module/model"
+	"github.com/whatsauth/wa"
+)
+
+func Post(w http.ResponseWriter, r *http.Request) {
+	var msg model.IteungMessage
+	var resp atmessage.Response
+	json.NewDecoder(r.Body).Decode(&msg)
+	link := "https://medium.com/@daffaaudyapramana03/cara-menggunakan-whatsauth-free-2fa-otp-notif-whatsapp-gateway-api-gratis-458105965936"
+	if r.Header.Get("Secret") == os.Getenv("SECRET") {
+		if msg.Message == "loc" || msg.Message == "Loc" || msg.Message == "lokasi" || msg.LiveLoc {
+			location, err := ReverseGeocode(msg.Latitude, msg.Longitude)
+			if err != nil {
+				// Handle the error (e.g., log it) and set a default location name
+				location = "Unknown Location"
+			}
+
+			reply := fmt.Sprintf("Aku ramal kamu pasti berada di %s \n Koordinatnya : %s - %s\n Tutorial WhatsAuth Ada di link dibawah ini"+
+				" yaa %s\n", location,
+				strconv.Itoa(int(msg.Longitude)), strconv.Itoa(int(msg.Latitude)), link)
+			dt := &wa.TextMessage{
+				To:       msg.Phone_number,
+				IsGroup:  false,
+				Messages: reply,
+			}
+			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
+		} else {
+			randm := []string{
+				"Hallo " + msg.Alias_name + "\n maaf kita sedang sibuk \n mohon tunggu \n nggaa lama kok \n",
+				"mohon jangan spam",
+				"untuk pemesanan kita belum tersedia mohon maaf",
+				"kita akan memperbaiki secepat mungkin, mohon ditunggu",
+				"kita akan segera kembali",
+			}
+			dt := &wa.TextMessage{
+				To:       msg.Phone_number,
+				IsGroup:  false,
+				Messages: GetRandomString(randm),
+			}
+			resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://api.wa.my.id/api/send/message/text")
+		}
+	} else {
+		resp.Response = "Secret Salah"
+	}
+	fmt.Fprintf(w, resp.Response)
+}
+
+func ReverseGeocode(latitude, longitude float64) (string, error) {
+	// OSM Nominatim API endpoint
+	apiURL := fmt.Sprintf("https://nominatim.openstreetmap.org/reverse?format=json&lat=%f&lon=%f", latitude, longitude)
+
+	// Make a GET request to the API
+	resp, err := http.Get(apiURL)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	// Decode the response
+	var result map[string]interface{}
+	err = json.NewDecoder(resp.Body).Decode(&result)
+	if err != nil {
+		return "", err
+	}
+
+	// Extract the place name from the response
+	displayName, ok := result["display_name"].(string)
+	if !ok {
+		return "", fmt.Errorf("unable to extract display_name from the API response")
+	}
+
+	return displayName, nil
+}
+
+func Liveloc(w http.ResponseWriter, r *http.Request) {
+	var msg model.IteungMessage
+	var resp atmessage.Response
+	json.NewDecoder(r.Body).Decode(&msg)
+
+	// Reverse geocode to get the place name
+	location, err := ReverseGeocode(msg.Latitude, msg.Longitude)
+	if err != nil {
+		// Handle the error (e.g., log it) and set a default location name
+		location = "Unknown Location"
+	}
+
+	reply := fmt.Sprintf("Aku sedang ramal kamu pasti berada di %s \n Koordinatenya : %s - %s\n", location,
+		strconv.Itoa(int(msg.Longitude)), strconv.Itoa(int(msg.Latitude)))
+
+	if r.Header.Get("Secret") == os.Getenv("SECRET") {
+		dt := &wa.TextMessage{
+			To:       msg.Phone_number,
+			IsGroup:  false,
+			Messages: reply,
+		}
+		resp, _ = atapi.PostStructWithToken[atmessage.Response]("Token", os.Getenv("TOKEN"), dt, "https://cloud.wa.my.id/api/send/message/text")
+	} else {
+		resp.Response = "Secret Salah"
+	}
+	fmt.Fprintf(w, resp.Response)
+}
+
+func GetRandomString(strings []string) string {
+	randomIndex := rand.Intn(len(strings))
+	return strings[randomIndex]
+}
